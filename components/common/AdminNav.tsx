2fcf45f4a202e23167f50855394dc54eee1584ce
leekoby <abc2752@naver.com>
2023-06-03 23:54:14 +0900

    refactor: AdminNav Menu

--- components/common/AdminNav.tsx
+++ components/common/AdminNav.tsx
@@ -1,20 +1,14 @@
 import Link from 'next/link';
 import { FC } from 'react';
 import Logo from './Logo';
-import {
-  AiOutlineDashboard,
-  AiOutlineContainer,
-  AiOutlineTeam,
-  AiOutlineMail,
-} from 'react-icons/ai';
 import { IconType } from 'react-icons';
 
 interface Props {
-  navItems: { label: string; icon: IconType };
+  navItems: { label: string; icon: IconType; href: string }[];
 }
 
 /** 2023/06/03 - AdminNavbar - by leekoby */
-const AdminNav: FC<Props> = (props): JSX.Element => {
+const AdminNav: FC<Props> = ({ navItems }): JSX.Element => {
   return (
     <nav className='h-screen w-60 shadow-sm bg-secondary-light dark:bg-secondary-dark'>
       {/* logo */}
@@ -26,37 +20,17 @@ const AdminNav: FC<Props> = (props): JSX.Element => {
       </Link>
       {/* nav items */}
       <div className={`space-y-6`}>
-        {/* 관리자 대시보드 */}
-        <Link
-          href={`/admin`}
-          className={`flex items-center text-highlight-light dark:text-highlight-dark text-xl p-3 hover:scale-[0.98] transition`}>
-          <AiOutlineDashboard size={24} />
-          <span className='ml-2'>DashBoard</span>
-        </Link>
-
-        {/* 글작성 */}
-        <Link
-          href={`/admin/posts`}
-          className={`flex items-center text-highlight-light dark:text-highlight-dark text-xl p-3 hover:scale-[0.98] transition`}>
-          <AiOutlineContainer size={24} />
-          <span className='ml-2'>Posts</span>
-        </Link>
-
-        {/* 사용자 */}
-        <Link
-          href={`/admin/users`}
-          className={`flex items-center text-highlight-light dark:text-highlight-dark text-xl p-3 hover:scale-[0.98] transition`}>
-          <AiOutlineTeam size={24} />
-          <span className='ml-2'>Users</span>
-        </Link>
-
-        {/* Mail */}
-        <Link
-          href={`/admin/comments`}
-          className={`flex items-center text-highlight-light dark:text-highlight-dark text-xl p-3 hover:scale-[0.98] transition`}>
-          <AiOutlineMail size={24} />
-          <span className='ml-2'>Comments</span>
-        </Link>
+        {navItems?.map((item) => {
+          return (
+            <Link
+              key={item.href}
+              href={item.href}
+              className={`flex items-center text-highlight-light dark:text-highlight-dark text-xl p-3 hover:scale-[0.98] transition`}>
+              <item.icon size={24} />
+              <span className='ml-2'>{item.label}</span>
+            </Link>
+          );
+        })}
       </div>
       {/* nav toggler (button) */}
     </nav>
--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,12 +1,26 @@
 import AdminNav from '@/components/common/AdminNav';
 import { NextPage } from 'next';
+import {
+  AiOutlineDashboard,
+  AiOutlineContainer,
+  AiOutlineTeam,
+  AiOutlineMail,
+  AiOutlineContacts,
+} from 'react-icons/ai';
 
 interface Props {}
 
+const navItems = [
+  { href: '/admin', icon: AiOutlineDashboard, label: 'DashBoard' },
+  { href: '/admin/posts', icon: AiOutlineContainer, label: 'Posts' },
+  { href: '/admin/users', icon: AiOutlineTeam, label: 'Users' },
+  { href: '/admin/comments', icon: AiOutlineMail, label: 'Comments' },
+  { href: '/admin/contact', icon: AiOutlineContacts, label: 'Contact' },
+];
 const Admin: NextPage<Props> = () => {
   return (
     <div className='dark'>
-      <AdminNav />
+      <AdminNav navItems={navItems} />
     </div>
   );
 };
