b96958a8e1db58701df61a56370a2689ed2284c3
leekoby <abc2752@naver.com>
2023-06-05 13:52:23 +0900

    feat: 게시글 에디터 문단, 제목 추가

---
+++ components/common/DropDownOption.tsx
@@ -0,0 +1,35 @@
+import { ReactNode, useState } from 'react';
+
+interface Props {
+  options: { label: string; onClick(): void }[];
+  head: ReactNode;
+}
+
+/** 2023/06/05 - 에디터 툴바 DropDown Menu - by leekoby */
+const DropdownOptions: React.FC<Props> = ({ head, options }): JSX.Element => {
+  const [showOptions, setShowOptions] = useState(false);
+
+  return (
+    <button
+      className='relative'
+      onBlur={() => setShowOptions(false)}
+      onMouseDown={() => setShowOptions(!showOptions)}>
+      {head}
+      {showOptions && (
+        <div className='min-w-max absolute top-full mt-4 right-2 z-10 border-2 border-primary-dark dark:border-primary rounded text-left bg-primary dark:bg-primary-dark'>
+          <ul className='p-3 space-y-3'>
+            {options.map(({ label, onClick }, index) => {
+              return (
+                <li key={label + index} onMouseDown={onClick}>
+                  {label}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      )}
+    </button>
+  );
+};
+
+export default DropdownOptions;
