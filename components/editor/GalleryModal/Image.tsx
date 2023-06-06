1bffbc8883f1b3ff6060115901cf8d77708c2c78
leekoby <abc2752@naver.com>
2023-06-06 21:14:37 +0900

    feat: 갤러리 모달 이미지 선택

--- components/editor/GalleryModal/Image.tsx
+++ components/editor/GalleryModal/Image.tsx
@@ -9,12 +9,7 @@ interface Props {
 const Image: React.FC<Props> = ({ src, selected, onClick }): JSX.Element => {
   return (
     <div onClick={onClick} className='rounded overflow-hidden cursor-pointer'>
-      <NextImage
-        src={src}
-        fill
-        alt='gallery'
-        className='bg-secondary-light hover:scale-110 transition p-1'
-      />
+      <NextImage src={src} fill alt='gallery' className=' hover:scale-105 transition p-2' />
       {/* png 업로드 때문에 bg 줌 */}
       <div className='absolute top-2 left-2'>
         <CheckMark visible={selected || false} />
--- components/editor/GalleryModal/index.tsx
+++ components/editor/GalleryModal/index.tsx
@@ -1,4 +1,7 @@
+import ActionButton from '@/components/common/ActionButton';
 import ModalContainer, { ModalProps } from '@/components/common/ModalContainer';
+import Image from 'next/image';
+import { useState } from 'react';
 import Gallery from './Gallery';
 
 interface Props extends ModalProps {}
@@ -80,16 +83,36 @@ const images = [
 
 /** 2023/06/06 - 이미지 첨부 모달 - by leekoby */
 const GalleryModal: React.FC<Props> = ({ visible, onClose }): JSX.Element => {
+  const [selectedImage, setSelectedImage] = useState('');
+
   return (
     <ModalContainer visible={visible} onClose={onClose}>
       <div className='max-w-4xl p-2 bg-primary-dark dark:bg-primary rounded'>
         <div className='flex'>
           {/* 갤러리 */}
           <div className='basis-3/4 max-h-[450px] overflow-y-auto custom-scroll-bar'>
-            <Gallery images={images} />
+            <Gallery
+              images={images}
+              onSelect={(src) => setSelectedImage(src)}
+              selectedImage={selectedImage}
+            />
           </div>
           {/* 이미지 선택 및 업로드 */}
-          <div className='basis-1/4'></div>
+          <div className='basis-1/4 px-2'>
+            <div className='space-y-4'>
+              {selectedImage ? (
+                <>
+                  <textarea
+                    className='resize-none w-full bg-transparent rounded border-2 border-secondary-dark focus:border-primary-dark focus:right-1 text-primary dark:text-primary-dark h-32 p-1'
+                    placeholder='alt text'></textarea>
+                  <ActionButton busy title='Select' />
+                  <div className='relative aspect-video bg-png-pattern'>
+                    <Image src={selectedImage} fill style={{ objectFit: 'contain' }} alt={''} />
+                  </div>
+                </>
+              ) : null}
+            </div>
+          </div>
         </div>
       </div>
     </ModalContainer>
