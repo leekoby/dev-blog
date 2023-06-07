855d02d17310d3ce9c75e59e03582b929f9d416f
leekoby <abc2752@naver.com>
2023-06-07 21:53:24 +0900

    feat: head

--- components/layout/AdminLayout.tsx
+++ components/layout/AdminLayout.tsx
@@ -9,9 +9,11 @@ import {
 } from 'react-icons/ai';
 import { ReactNode } from 'react';
 import Link from 'next/link';
+import AppHead from '../common/AppHead';
 
 interface Props {
   children: ReactNode;
+  title?: string;
 }
 
 const navItems = [
@@ -22,18 +24,21 @@ const navItems = [
   { href: '/admin/contact', icon: AiOutlineContacts, label: 'Contact' },
 ];
 
-const AdminLayout: React.FC<Props> = ({ children }): JSX.Element => {
+const AdminLayout: React.FC<Props> = ({ children, title }): JSX.Element => {
   return (
-    <div className='flex'>
-      <AdminNav navItems={navItems} />
-      <div className='flex-1 p-4'>{children}</div>
-      {/* create button */}
-      <Link
-        href={'/admin/post/create'}
-        className={`bg-secondary-dark dark:bg-secondary-light text-primary dark:text-primary-dark fixed z-10 right-10 bottom-10 p-3 rounded-full hover:scale-90 shadow-sm transition`}>
-        <AiOutlineFileAdd size={24} />
-      </Link>
-    </div>
+    <>
+      <AppHead title={title} />
+      <div className='flex'>
+        <AdminNav navItems={navItems} />
+        <div className='flex-1 p-4'>{children}</div>
+        {/* create button */}
+        <Link
+          href={'/admin/post/create'}
+          className={`bg-secondary-dark dark:bg-secondary-light text-primary dark:text-primary-dark fixed z-10 right-10 bottom-10 p-3 rounded-full hover:scale-90 shadow-sm transition`}>
+          <AiOutlineFileAdd size={24} />
+        </Link>
+      </div>
+    </>
   );
 };
 
--- pages/admin/post/create.tsx
+++ pages/admin/post/create.tsx
@@ -1,18 +1,21 @@
 import Editor from '@/components/editor';
+import AdminLayout from '@/components/layout/AdminLayout';
 import { NextPage } from 'next';
 
 interface Props {}
 
 const Create: NextPage<Props> = () => {
   return (
-    <div className='max-w-4xl mx-auto'>
-      <Editor
-        onSubmit={(post) => {
-          console.log(post);
-        }}
-        // initialValue={{}}
-      />
-    </div>
+    <AdminLayout title='New Post'>
+      <div className='max-w-4xl mx-auto'>
+        <Editor
+          onSubmit={(post) => {
+            console.log(post);
+          }}
+          // initialValue={{}}
+        />
+      </div>
+    </AdminLayout>
   );
 };
 
