f8aba7a40f9eb35b85934dd9c2319ba79da89ad7
leekoby <abc2752@naver.com>
2023-06-15 20:04:39 +0900

    feat: 댓글 Shema 유효성 검사

--- lib/validator.ts
+++ lib/validator.ts
@@ -1,4 +1,5 @@
 import Joi, { ObjectSchema } from 'joi';
+import { isValidObjectId } from 'mongoose';
 
 /** 2023/06/08 -  유효성 검사 커스텀 메세지 - by leekoby */
 export const errorMessages = {
@@ -6,7 +7,7 @@ export const errorMessages = {
   INVALID_TAGS: '태그는 문자열 배열입니다.',
   INVALID_SLUG: 'Slug가 없어요.',
   INVALID_META: 'Meta 설명을 잊으셨군요.',
-  INVALID_CONTENT: '본문을 작성해주세요.',
+  INVALID_CONTENT: '내용을 작성해주세요.',
 };
 /** 2023/06/08 - 스키마 유효성 검사 변수 - by leekoby */
 export const postValidationSchema = Joi.object().keys({
@@ -32,7 +33,8 @@ export const postValidationSchema = Joi.object().keys({
     'string.empty': errorMessages.INVALID_TAGS,
   }),
 });
-/** 2023/06/08 - 스키마 유효성 검사 함수 - by leekoby */
+
+/** 2023/06/08 - 스키마 유효성 검사 - by leekoby */
 export const validateSchema = (schema: ObjectSchema, value: any) => {
   const { error } = schema.validate(value, {
     errors: { label: 'key', wrap: { label: false, array: false } },
@@ -41,3 +43,20 @@ export const validateSchema = (schema: ObjectSchema, value: any) => {
   if (error) return error.details[0].message;
   return '';
 };
+
+/** 2023/06/11 - 댓글 스키마 유효성 검사 - by leekoby */
+export const commentValidationSchema = Joi.object().keys({
+  belongsTo: Joi.string()
+    .required()
+    .custom((value, helper) => {
+      if (!isValidObjectId(value)) return helper.error('any.invalid');
+      return true;
+    })
+    .messages({
+      'any.invalid': '게시물 ID는 소속으로 표시해야 합니다!',
+      'string.empty': '유효하지 않은 소속',
+    }),
+  content: Joi.string().required().messages({
+    'string.empty': '댓글의 내용이 누락되었습니다.',
+  }),
+});
--- pages/api/comment/index.ts
+++ pages/api/comment/index.ts
@@ -1,4 +1,5 @@
 import { isAuth } from '@/lib/utils';
+import { commentValidationSchema, validateSchema } from '@/lib/validator';
 import { NextApiHandler } from 'next';
 
 /** 2023/06/09 - 댓글  API - by leekoby */
@@ -14,8 +15,13 @@ const handler: NextApiHandler = (req, res) => {
 
 /** 2023/06/09 - 댓글 등록 API - by leekoby */
 const createNewComment: NextApiHandler = async (req, res) => {
-  const user = await isAuth(req, res);
-  if (!user) return res.status(403).json({ error: 'unauthrized request' });
+  // const user = await isAuth(req, res);
+  // if (!user) return res.status(403).json({ error: 'unauthrized request' });
+
+  const error = validateSchema(commentValidationSchema, req.body);
+
+  if (error) return res.status(422).json({ error });
+  // validate를 통과하지 못할때  https://developer.mozilla.org/ko/docs/Web/HTTP/Status/422
 };
 
 export default handler;
