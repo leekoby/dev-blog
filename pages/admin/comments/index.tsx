3fc3f2368abda9f414160ce796bb67927ae844d1
leekoby <abc2752@naver.com>
2023-06-25 14:18:53 +0900

    design: 관리자 댓글 페이지 제목 추가

--- pages/admin/comments/index.tsx
+++ pages/admin/comments/index.tsx
@@ -7,7 +7,12 @@ interface Props {}
 const AdminComments: NextPage<Props> = () => {
   return (
     <AdminLayout>
-      <Comments fetchAll />
+      <div className='max-w-4xl mx-auto'>
+        <h1 className='text-2xl text-primary-dark dark:text-primary font-semibold py-2 transition'>
+          Comments
+        </h1>
+        <Comments fetchAll />
+      </div>
     </AdminLayout>
   );
 };
