73528502c14b1ad344a3342c8ba8bc48c3bb277d
leekoby <abc2752@naver.com>
2023-06-25 21:05:24 +0900

    fix: UserNav 구현된 부분 Props 수정

--- components/common/nav/UserNav.tsx
+++ components/common/nav/UserNav.tsx
@@ -48,9 +48,9 @@ const UserNav: React.FC<Props> = (props): JSX.Element => {
   return (
     <div className='flex items-center justify-between bg-primary-dark p-3'>
       {/* Logo */}
-      <Link href={'/'} className='flex space-x-2 text-highlight-dark'>
-        <Logo className='fill-highlight-dark' />
-        <span className='text-xl font-semibold'>{APP_NAME}</span>
+      <Link href={'/'} className='flex items-center space-x-2 text-highlight-dark'>
+        <Logo className='fill-highlight-dark md:w-8 md:h-8 w-5 h-5' />
+        <span className='md:text-xl font-semibold'>{APP_NAME}</span>
       </Link>
       <div className='flex items-center space-x-5'>
         <button onClick={toggleTheme} className='dark:text-secondary-dark text-secondary-light'>
@@ -60,7 +60,13 @@ const UserNav: React.FC<Props> = (props): JSX.Element => {
         {isAuth ? (
           <DropdownOptions
             options={dropDownOptions}
-            head={<ProfileHead nameInitial='N' lightOnly />}
+            head={
+              <ProfileHead
+                nameInitial={profile?.name[0].toUpperCase()}
+                avatar={profile?.avatar}
+                lightOnly
+              />
+            }
           />
         ) : (
           <GitHubAuthButton lightOnly />
