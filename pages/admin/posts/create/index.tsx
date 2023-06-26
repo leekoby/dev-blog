5327f173e6e21be6e5b47434117d8f276ca118da
leekoby <abc2752@naver.com>
2023-06-27 02:11:10 +0900

    feat: 게시글 제출 후 작성글로 이동

--- pages/admin/posts/create/index.tsx
+++ pages/admin/posts/create/index.tsx
@@ -3,10 +3,13 @@ import AdminLayout from '@/components/layout/AdminLayout';
 import { generateFormData } from '@/utils/helper';
 import axios from 'axios';
 import { NextPage } from 'next';
+import { useRouter } from 'next/router';
 
 interface Props {}
 /** 2023/06/05 - 게시글 생성 - by leekoby */
 const Create: NextPage<Props> = () => {
+  const router = useRouter();
+
   /** 2023/06/08 - 게시글 제출  - by leekoby */
   const handleSubmit = async (post: FinalPost) => {
     try {
@@ -15,6 +18,7 @@ const Create: NextPage<Props> = () => {
 
       // 게시글 제출
       const { data } = await axios.post('/api/posts', formData);
+      router.push('/' + data.post.slug);
     } catch (error: any) {
       console.log(error.response.data);
     }
