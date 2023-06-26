57cfd3a7003d791a539a0640f836ebbd16cbf5d3
leekoby <abc2752@naver.com>
2023-06-27 02:10:16 +0900

    fix: 편의성을 위한 홈 이동 추가

--- components/common/nav/AdminNav.tsx
+++ components/common/nav/AdminNav.tsx
@@ -7,6 +7,7 @@ import Logo from '../Logo';
 
 import Tippy from '@tippyjs/react';
 import 'tippy.js/dist/tippy.css';
+import { AiOutlineHome } from 'react-icons/ai';
 
 interface Props {
   navItems: { label: string; icon: IconType; href: string }[];
@@ -60,16 +61,29 @@ const AdminNav: React.FC<Props> = ({ navItems }): JSX.Element => {
       ref={navRef}
       className='flex flex-col justify-between h-screen w-60 shadow-sm bg-secondary-light dark:bg-secondary-dark transition-width overflow-hidden sticky top-0'>
       <div>
-        {/* logo */}
-        <Link href={`/admin`} className={`flex items-center space-x-2 p-3 mb-10`}>
-          <Logo className={`fill-highlight-light dark:fill-highlight-dark w-5 h-5`} />
-          {visible && (
-            <span
-              className={`text-highlight-light dark:text-highlight-dark text-xl font-semibold leading-none`}>
-              Admin
-            </span>
-          )}
-        </Link>
+        <div className='space-y-6 mb-10'>
+          {/* logo */}
+
+          <Link href={`/admin`} className={`flex items-center space-x-2 p-3 `}>
+            <Logo className={`fill-highlight-light dark:fill-highlight-dark w-5 h-5`} />
+            {visible && (
+              <span
+                className={`text-highlight-light dark:text-highlight-dark text-xl font-semibold leading-none`}>
+                Admin
+              </span>
+            )}
+          </Link>
+          <Link href={`/`} className={`flex items-center space-x-2 p-3 `}>
+            {/* <Logo className={`fill-highlight-light dark:fill-highlight-dark w-5 h-5`} /> */}
+            <AiOutlineHome className={`fill-highlight-light dark:fill-highlight-dark w-5 h-5`} />
+            {visible && (
+              <span
+                className={`text-highlight-light dark:text-highlight-dark text-xl font-semibold leading-none`}>
+                Home
+              </span>
+            )}
+          </Link>
+        </div>
         {/* nav items */}
         <div className={`space-y-6`}>
           {navItems?.map((item) => {
