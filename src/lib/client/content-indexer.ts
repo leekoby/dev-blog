ccfc06c2b7577b0c94ebe91023e4768627b56823
leekoby <abc2752@naver.com>
2023-06-30 23:46:12 +0900

    feat: JsSearch Class 제작

---
+++ src/lib/client/content-indexer.ts
@@ -0,0 +1,31 @@
+import searchIndex from '@/content/search/index.json';
+import * as JsSearch from 'js-search';
+
+/** 2023/06/30 - JsSearch Class - by leekoby */
+class ContentIndexer {
+  private static instance: ContentIndexer;
+
+  //definite assignment asserstion
+  private searchEngine!: JsSearch.Search;
+
+  public static get Instance() {
+    return this.instance || (this.instance = new this());
+  }
+
+  constructor() {
+    this.buildIndex();
+  }
+
+  public search(query: string) {
+    const results = this.searchEngine.search(query);
+    return results;
+  }
+  private buildIndex() {
+    this.searchEngine = new JsSearch.Search('slug');
+    this.searchEngine.addIndex('title');
+    this.searchEngine.addIndex('description');
+    this.searchEngine.addDocuments(searchIndex);
+  }
+}
+
+export default ContentIndexer.Instance;
